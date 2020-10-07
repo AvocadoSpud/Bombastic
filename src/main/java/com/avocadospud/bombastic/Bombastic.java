d38e9539bc1ffcdf161a0514e2530c4bd437e143
AvocadoSpud <69398022+AvocadoSpud@users.noreply.github.com>
2020-10-07 11:26:45 -0400

    Update Bombastic.java
    q

--- src/main/java/com/avocadospud/bombastic/Bombastic.java
+++ src/main/java/com/avocadospud/bombastic/Bombastic.java
@@ -20,7 +20,7 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("bombastic")
+@Mod(Bombastic.MOD_ID)
 public class Bombastic {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
